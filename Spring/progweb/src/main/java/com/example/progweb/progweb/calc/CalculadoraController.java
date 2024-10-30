6e29f040a00ad7972c6e892f21e4da742c69a092
Amanda Gois <amandabalcacar@gmail.com>
2024-10-30 14:02:45 -0400

    feat: calculadora simples com método get

---
+++ Spring/progweb/src/main/java/com/example/progweb/progweb/calc/CalculadoraController.java
@@ -0,0 +1,33 @@
+package com.example.progweb.progweb.calc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
+ public class CalculadoraController {  
+
+  @GetMapping("/soma")
+  @ResponseBody  
+  public String somaGet(
+    @RequestParam(name = "a", defaultValue = "0") int a, 
+    @RequestParam(name = "b", defaultValue = "0") int b)
+    
+    {int result = Calculadora.soma(a, b);
+      return "<p>(GET) o resultado da soma é: "+result+"</p>";  
+    }
+
+    @PostMapping("/soma")
+    @ResponseBody  
+    public String somaPost(
+      @RequestParam(name = "a", defaultValue = "0") int a, 
+      @RequestParam(name = "b", defaultValue = "0") int b)
+      
+      {int result = Calculadora.soma(a, b);
+        return "<p>(POST) o resultado da soma é: "+result+"</p>";  
+      }
+ }
--- Spring/progweb/src/main/java/com/example/progweb/progweb/hello/HelloController.java
+++ Spring/progweb/src/main/java/com/example/progweb/progweb/hello/HelloController.java
@@ -3,14 +3,22 @@ package com.example.progweb.progweb.hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping
  public class HelloController {  
-  @GetMapping  
+  @GetMapping("/hello")
   @ResponseBody  
   public String index(){    
    return "Hello World!!!";  
- }  
+ }
+
+ @GetMapping("/hello2")
+  @ResponseBody  
+  public String index2(@RequestParam(name = "n", defaultValue = "World") String n){
+   return "<p>Hello " + n + "!</p>";  
+ }
+ 
 }
